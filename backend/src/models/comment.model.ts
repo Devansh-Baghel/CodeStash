58d69ec8c78a2d7e05673816e734e38ac5c8ee2c
Devansh Baghel <77718741+Devansh-Baghel@users.noreply.github.com>
2024-05-17 20:58:11 +0530

    backend: add comment model

---
+++ backend/src/models/comment.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface CommentTypes extends Document {
+  madeBy: Schema.Types.ObjectId;
+  parent: Schema.Types.ObjectId;
+  type: "comment" | "reply";
+  content: string;
+  upvotes: number;
+  downvotes: number;
+}
+
+const commentSchema: Schema<CommentTypes> = new Schema(
+  {
+    madeBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment" || "Snippet",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: "comment",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    upvotes: {
+      type: Number,
+      default: 0,
+    },
+    downvotes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
