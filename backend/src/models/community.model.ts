a7988e53b0600821de61009f5ca5082834885b22
Devansh Baghel <devanshbaghel85@gmail.com>
2024-06-17 11:15:37 +0530

    BE: Add community model

---
+++ backend/src/models/community.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface CommunityTypes extends Document {
+  madeBy: {
+    userId: Schema.Types.ObjectId;
+    username: string;
+  };
+  name: string;
+  joinedMembers: number;
+  coverImage: string;
+  avatar: string;
+}
+
+const communitySchema: Schema<CommunityTypes> = new Schema(
+  {
+    madeBy: {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+      },
+      username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+      },
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    joinedMembers: {
+      type: Number,
+      default: 0,
+    },
+    coverImage: { type: String },
+    avatar: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const Community = mongoose.model("Community", communitySchema);
