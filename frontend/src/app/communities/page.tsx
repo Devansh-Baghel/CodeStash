73004aaf54914c30b5bd55d1ea4f39ece869a0bd
Devansh Baghel <devanshbaghel85@gmail.com>
2024-06-19 16:19:05 +0530

    FE: add communities ui

--- frontend/src/app/communities/page.tsx
+++ frontend/src/app/communities/page.tsx
@@ -9,11 +9,24 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import fetcher from "@/utils/axios";
+import { Button } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+
+export type CommunityTypes = {
+  madeBy: {
+    userId: string;
+    username: string;
+  };
+  _id: string;
+  name: string;
+  description: string;
+  joinedMembers: number;
+};
 
 export default function () {
   // TODO: add community types
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<CommunityTypes[]>({
     queryKey: ["communities"],
     queryFn: async () => {
       return await fetcher.get("/community/get-all");
@@ -26,12 +39,37 @@ export default function () {
   console.log(data);
 
   return (
-    <>
-      {data.map((community) => (
+    <section className="flex flex-col gap-4">
+      {data?.map((community) => (
         <Card key={community._id}>
-          <CardHeader>{community.name}</CardHeader>
+          <CardHeader className="flex flex-row items-center justify-between">
+            <Link href={`c/${community.name}`}>
+              <CardTitle className="mb-2 text-lg">c/{community.name}</CardTitle>
+              <CardDescription>{community.description}</CardDescription>
+            </Link>
+            <div className="flex flex-col items-center">
+              <p className="text-sm">{community.joinedMembers} members</p>
+              <Button color="primary" radius="md">
+                + Join community
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            {/* TODO: add community cover image and avatar here */}
+          </CardContent>
+          <CardFooter>
+            <CardDescription>
+              Community created by:{" "}
+              <Link
+                href={`u/${community.madeBy.username}`}
+                className="underline"
+              >
+                u/{community.madeBy.username}
+              </Link>
+            </CardDescription>
+          </CardFooter>
         </Card>
       ))}
-    </>
+    </section>
   );
 }
