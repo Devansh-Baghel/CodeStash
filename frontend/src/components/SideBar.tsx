216c18cda1045dacc44d2007bb9a9c825f5ae23d
Devansh Baghel <devanshbaghel85@gmail.com>
2024-06-23 16:19:36 +0530

    sidebar changes

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,14 +1,34 @@
+import { useUserStore } from "@/store/userStore";
+import { Listbox, ListboxItem } from "@nextui-org/listbox";
+
 export default function SideBar() {
+  const { isLoggedIn } = useUserStore();
+
   return (
-    <aside className="hidden min-w-80 md:block">
+    <aside className="hidden min-w-80 py-10 pl-4 pr-20 md:block">
       {/* TODO: Make the reddit like sidebar */}
-      This is aside
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      {!isLoggedIn ? (
+        // Link of items and pages
+        // Home
+        // Popular langugages
+        // Communities
+        // Popular communities
+        // FAQ page
+        // Settings page
+        // Profile
+        <Listbox aria-label="Actions" onAction={(key) => alert(key)}>
+          <ListboxItem key="new">New file</ListboxItem>
+          <ListboxItem key="copy">Copy link</ListboxItem>
+          <ListboxItem key="edit">Edit file</ListboxItem>
+          <ListboxItem key="delete" className="text-danger" color="danger">
+            Delete file
+          </ListboxItem>
+        </Listbox>
+      ) : (
+        <div>
+          <h1>This is aside</h1>
+        </div>
+      )}
     </aside>
   );
 }
