e53e1109287c19b78bbf262f48aaa2c058492096
Devansh Baghel <devanshbaghel85@gmail.com>
2024-07-03 16:23:32 +0530

    FE: add update comment feature

--- frontend/src/components/CommentItem.tsx
+++ frontend/src/components/CommentItem.tsx
@@ -8,6 +8,10 @@ import { Comment } from "@/types/commentTypes";
 import React, { useState } from "react";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "next/navigation";
+import { MdDelete as DeleteIcon } from "react-icons/md";
+import { Textarea } from "@nextui-org/react";
+// import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 function CommentItem({
   comment,
@@ -21,9 +25,13 @@ function CommentItem({
   const [upvoteCount, setUpvoteCount] = useState(
     comment.upvotes - comment.downvotes,
   );
+  const [updatedContent, setUpdatedContent] = useState(comment.content);
+  const [isUpdating, setIsUpdating] = useState(false);
   const router = useRouter();
 
-  function handleInteraction(commentId: string, action: "upvote" | "downvote") {
+  function handleInteraction(action: "upvote" | "downvote") {
+    const commentId = comment._id;
+
     if (!isLoggedIn) {
       router.push("/login");
       return;
@@ -54,6 +62,8 @@ function CommentItem({
     }
   }
 
+  function deleteComment() {}
+
   //   FIXME: comment count is cached after an upvote/downvote and user has to refresh to get the accurate count
   return (
     <li key={comment._id} className="flex gap-2 rounded-xl bg-primary-50 p-2">
@@ -62,38 +72,77 @@ function CommentItem({
         {userData?.upvotedComments.includes(comment._id) ? (
           <SolidUpvoteIcon
             className="h-5 w-5 cursor-pointer"
-            onClick={() => handleInteraction(comment._id, "upvote")}
+            onClick={() => handleInteraction("upvote")}
           />
         ) : (
           <UpvoteIcon
             className="h-5 w-5 cursor-pointer"
-            onClick={() => handleInteraction(comment._id, "upvote")}
+            onClick={() => handleInteraction("upvote")}
           />
         )}
         {upvoteCount}
         {userData?.downvotedComments.includes(comment._id) ? (
           <SolidDownvoteIcon
             className="h-5 w-5 cursor-pointer"
-            onClick={() => handleInteraction(comment._id, "downvote")}
+            onClick={() => handleInteraction("downvote")}
           />
         ) : (
           <DownvoteIcon
             className="h-5 w-5 cursor-pointer"
-            onClick={() => handleInteraction(comment._id, "downvote")}
+            onClick={() => handleInteraction("downvote")}
           />
         )}
       </div>
-      <div>
-        <Link
-          href={`/u/${comment.madeBy.username}`}
-          className="text-grey-700 text-xs"
-        >
-          u/{comment.madeBy.username}
-          {comment.madeBy.username === madeBy && (
-            <Badge className="ml-2">OP</Badge>
+      <div className="w-full">
+        <div className="flex w-full justify-between">
+          <Link
+            href={`/u/${comment.madeBy.username}`}
+            className="text-grey-700 text-xs"
+          >
+            u/{comment.madeBy.username}
+            {comment.madeBy.username === madeBy && (
+              <Badge className="ml-2 text-xs font-normal">OP</Badge>
+            )}
+          </Link>
+
+          {comment.madeBy.username === userData?.username && !isUpdating && (
+            <div className="flex items-center gap-2 text-xs">
+              <Badge
+                className="hover:cursor-pointer"
+                variant="default"
+                onClick={() => setIsUpdating(true)}
+              >
+                Update
+              </Badge>
+              <DeleteIcon className="h-5 w-5 hover:cursor-pointer" />
+            </div>
           )}
-        </Link>
-        <p>{comment.content}</p>
+        </div>
+        {isUpdating ? (
+          <form>
+            <Textarea
+              variant="bordered"
+              color="primary"
+              className="my-2"
+              required
+              value={updatedContent}
+              onChange={(e) => setUpdatedContent(e.target.value)}
+            />
+            <div className="flex gap-2">
+              <Button size="sm">Save changes</Button>
+              <Button
+                size="sm"
+                variant="outline"
+                type="button"
+                onClick={() => setIsUpdating(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        ) : (
+          <p>{comment.content}</p>
+        )}
       </div>
     </li>
   );
