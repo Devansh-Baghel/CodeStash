00d6a4e3f33159e744ff1a3dd9207ca575caab39
Devansh Baghel <devanshbaghel85@gmail.com>
2024-07-18 03:14:29 +0530

    BE: add change password controller

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -175,7 +175,7 @@ export const updateUsername = asyncHandler(async (req: UserRequest, res) => {
   if (userExists)
     throw new ApiError(409, "User with this username already exists");
 
-  user.username = newUsername;
+  user!.username = newUsername;
   await user?.save();
 
   return res
@@ -214,3 +214,26 @@ export const uploadAvatar = asyncHandler(async (req: UserRequest, res) => {
       new ApiResponse(200, { user: updatedUser }, "Avatar uploaded sucessfully")
     );
 });
+
+export const changeCurrentPassword = asyncHandler(
+  async (req: UserRequest, res) => {
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword)
+      throw new ApiError(400, "Both old and new passwords are required");
+
+    const user = await User.findById(req.user?._id);
+    const isPasswordCorrect = await user?.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+      throw new ApiError(401, "Invalid old password");
+    }
+
+    user!.password = newPassword;
+    await user?.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Password changed successfully"));
+  }
+);
