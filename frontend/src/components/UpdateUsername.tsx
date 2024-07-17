e97af48bfdd9bd63e9a7d7da81c901fdbcb9cdd6
Devansh Baghel <devanshbaghel85@gmail.com>
2024-07-18 03:08:43 +0530

    FE: add loading spinner on button during update username mutation

--- frontend/src/components/UpdateUsername.tsx
+++ frontend/src/components/UpdateUsername.tsx
@@ -5,7 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import { BiLoaderAlt as Loader } from "react-icons/bi";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -13,26 +13,32 @@ import fetcher from "@/utils/axios";
 import { FormEvent, useState } from "react";
 import { toast } from "./ui/use-toast";
 import { useUserStore } from "@/store/userStore";
+import { useMutation } from "@tanstack/react-query";
 
 export default function UpdateUsername() {
   const [newUsername, setNewUsername] = useState("");
   const { setUserData } = useUserStore();
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["update-username"],
+    mutationFn: () => {
+      return fetcher
+        .patch("/users/update-username", {
+          newUsername,
+        })
+        .then((res) => {
+          setUserData(res.updatedUser);
+          setNewUsername("");
 
-  async function updateUsername(e: FormEvent) {
-    e.preventDefault();
-
-    await fetcher
-      .patch("/users/update-username", {
-        newUsername,
-      })
-      .then((res) => {
-        setUserData(res.updatedUser);
-        setNewUsername("");
-
-        toast({
-          description: "Username updated successfully",
+          toast({
+            description: "Username updated successfully",
+          });
         });
-      });
+    },
+  });
+
+  function updateUsername(e: FormEvent) {
+    e.preventDefault();
+    mutate();
   }
 
   return (
@@ -54,7 +60,8 @@ export default function UpdateUsername() {
                 value={newUsername}
                 onChange={(e) => setNewUsername(e.target.value)}
               />
-              <Button type="submit" className="w-60">
+              <Button type="submit" className="w-80" disabled={isPending}>
+                {isPending && <Loader className="mr-2 h-4 w-4 animate-spin" />}
                 Update Username
               </Button>
             </div>
