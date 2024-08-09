bd21a9b1841f2927741792c7d4bc45793aadb7bd
Devansh Baghel <devanshbaghel85@gmail.com>
2024-08-09 23:44:47 +0530

    FE: community info card ui

--- frontend/src/app/c/[community]/page.tsx
+++ frontend/src/app/c/[community]/page.tsx
@@ -1,35 +1,65 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CommunityTypes } from "@/app/communities/page";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import fetcher from "@/utils/axios";
 import { cardLayout } from "@/utils/classnames";
 import { Avatar } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default function Page({ params }: { params: { community: string } }) {
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<CommunityTypes>({
     queryKey: [`c/${params.community}`],
     queryFn: () => {
+      // TODO: if community does not exist, show error page.
       return fetcher.post("/community/get-community", {
         name: params.community,
       });
     },
   });
 
-  if (isError) return "Error";
   // FIXME: Loading screen here
   if (isLoading) return "Loading...";
+  if (isError || !data) return "Error";
+
+  console.log(data);
 
   return (
     <section className={cn(cardLayout)}>
-      <Card>
+      <Card className="border-t-5 border-primary">
         <CardHeader>
-          <CardTitle>
-            <Avatar src="" className="" />
-          </CardTitle>
+          <CardTitle></CardTitle>
+          <div className="ml-4 flex gap-4">
+            <Avatar src="" className="h-16 w-16" name={data.name} size="lg" />
+            <div>
+              <h1 className="text-xl font-semibold">c/{data.name}</h1>
+              <p className="">{data.description}</p>
+            </div>
+          </div>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent className="pb-3 text-center text-sm">
+          <p>
+            {data.joinedMembers}{" "}
+            {data.joinedMembers === 1 ? "member" : "members"}
+          </p>
+          <p>
+            community created by{" "}
+            <Link
+              href={`/u/${data.madeBy.username}`}
+              className="text-primary underline"
+            >
+              c/{data.madeBy.username}
+            </Link>
+          </p>
+        </CardContent>
       </Card>
     </section>
   );
