31be56169cafc297ec1f89a9b2e3dbed281880d1
Devansh Baghel <devanshbaghel85@gmail.com>
2024-08-23 14:26:50 +0530

    FE: uploading avatar works now

--- frontend/src/components/UploadAvatar.tsx
+++ frontend/src/components/UploadAvatar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import {
   Modal,
   ModalContent,
@@ -9,13 +9,53 @@ import {
   useDisclosure,
   Input,
 } from "@nextui-org/react";
+import toast from "react-hot-toast";
+import { axiosInstance } from "@/utils/axios";
+import { useUserStore } from "@/store/userStore";
 
 export default function UploadAvatar({ buttonText }: { buttonText: string }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [avatar, setAvatar] = useState<File>();
+  const { setUserData } = useUserStore();
+
+  function uploadAvatar(e: FormEvent) {
+    e.preventDefault();
+
+    if (!avatar) {
+      toast.error("Please upload avatar file");
+      return;
+    }
+
+    if (!["image/jpeg", "image/png"].includes(avatar?.type)) {
+      toast.error("Only jpeg and png files are allowed");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("avatar", avatar);
+
+    const toastPromise = axiosInstance
+      .post("/users/upload-avatar", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        setUserData(res.data.data.user);
+      });
+
+    toast.promise(toastPromise, {
+      loading: "Saving Avatar",
+      success: "Saved Avatar",
+      error: "Error saving avatar",
+    });
+  }
 
   return (
     <>
-      <Button onPress={onOpen}>{buttonText}</Button>
+      <Button onPress={onOpen} color="primary">
+        {buttonText}
+      </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
@@ -23,9 +63,18 @@ export default function UploadAvatar({ buttonText }: { buttonText: string }) {
               <ModalHeader className="flex flex-col gap-1">
                 {buttonText}
               </ModalHeader>
-              <form>
+              <form onSubmit={uploadAvatar}>
                 <ModalBody>
-                  <Input type="file" required />
+                  <Input
+                    type="file"
+                    id="avatar"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(e) => {
+                      if (!e.target.files) return;
+                      setAvatar(e.target.files[0]);
+                    }}
+                  />
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={onClose}>
