092cdd5ef476900d7c8a7830ce3f733080644c54
Devansh Baghel <devanshbaghel85@gmail.com>
2024-08-25 21:45:50 +0530

    BE: update imports in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,20 +3,13 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 
-import commentRouter from "./routes/comment.routes";
-import communityRouter from "./routes/community.routes";
-// Routes
-import healthCheckRouter from "./routes/healthCheck.routes";
-import postRouter from "./routes/post.routes";
-import userRouter from "./routes/user.routes";
-
 const app = express();
 
 app.use(
   cors({
     origin: [process.env.CORS_ORIGIN!],
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json({ limit: "16kb" }));
@@ -24,6 +17,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(helmet());
+
+// Routes
+import commentRouter from "./routes/comment.routes";
+import communityRouter from "./routes/community.routes";
+import healthCheckRouter from "./routes/healthCheck.routes";
+import postRouter from "./routes/post.routes";
+import userRouter from "./routes/user.routes";
 
 app.use("/api", healthCheckRouter);
 app.use("/api/users", userRouter);
