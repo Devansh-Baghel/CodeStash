ad586d164f58291dc787fd44f9f338b289c73f1d
Devansh Baghel <devanshbaghel85@gmail.com>
2024-09-23 12:29:34 +0530

    fe: refactor deletepostbutton to use nextui modals

--- frontend/src/components/buttons/DeletePostButton.tsx
+++ frontend/src/components/buttons/DeletePostButton.tsx
@@ -1,21 +1,20 @@
 import { Button as ShadButton } from "@/components/ui/button";
 import { MdDelete as DeleteIcon } from "react-icons/md";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import fetcher from "@/utils/axios";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
 
 export default function DeletePostButton({ postId }: { postId: string }) {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
   const router = useRouter();
 
   function deletePost() {
@@ -30,35 +29,46 @@ export default function DeletePostButton({ postId }: { postId: string }) {
       success: "Deleted post successfully",
       error: "Failed to delete post",
     });
+
+    onClose();
   }
 
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <ShadButton
-          className="text-sm text-black hover:text-red-500"
-          variant="link"
-          size="sm"
-        >
-          <DeleteIcon className="mr-1 size-4" />
-          Delete Post
-        </ShadButton>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Are you sure that you want to delete this post?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your post
-            and all its comments.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={deletePost}>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <>
+      <ShadButton
+        className="text-sm text-black hover:text-red-500"
+        variant="link"
+        size="sm"
+        onClick={onOpen}
+      >
+        <DeleteIcon className="mr-1 size-4" />
+        Delete Post
+      </ShadButton>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Are you sure that you want to delete this post?
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  This action cannot be undone. This will permanently delete
+                  your post and all its comments.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onClick={deletePost}>
+                  Delete Post
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
