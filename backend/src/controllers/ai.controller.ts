4e7249b1d7852be08dd78aa56337c981873479c5
Devansh Baghel <devanshbaghel85@gmail.com>
2025-09-30 11:10:01 +0530

    chore: use lite model for better reqests per day limits

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -41,7 +41,7 @@ export const getAiAnswer = asyncHandler(async (req: UserRequest, res) => {
   const genAI = new GoogleGenerativeAI(apiKey);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.5-flash-lite",
   });
 
   const generationConfig = {
